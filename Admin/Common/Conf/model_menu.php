0a847d026c16e1d1e4eb88da872ea2316a665fe3
simon <mahuan1768@126.com>
2015-03-03 22:13:41 +0800

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   .gitignore
    	modified:   .gitignore~
    	new file:   Admin/Common/Conf/fill_config.php
    	new file:   Admin/Common/Conf/filter_config.php
    	new file:   Admin/Common/Conf/model_menu.php
    	modified:   Public/javascripts/home/jquery.mousewheel.js
    	modified:   Public/javascripts/home/jquery.scrollAdvance.js
    	modified:   Public/stylesheets/home/main.css
    	modified:   Public/stylesheets/home/sprite.png
    	modified:   Public/uploads/HappyArticle/54f565d1bb55f.png
    	modified:   Public/uploads/HappySee/54f521168e1aa.png
    	modified:   Public/uploads/HappySee/54f5212a810b6.png
    	modified:   Public/uploads/HappySee/54f5213226d1d.png
    	modified:   Public/uploads/HappySee/54f521386dc71.png
    	modified:   Public/uploads/ProduceImg/54f50682b4f46.png
    	modified:   Public/uploads/ProduceImg/54f506e38d792.png

---
+++ Admin/Common/Conf/model_menu.php
@@ -0,0 +1,123 @@
+<?php
+return array (
+  'HomeImg' => 
+  array (
+    'name' => '首页图片管理',
+    'target' => 'HomeImg/index',
+    'sub_menu' => 
+    array (
+      0 => 
+      array (
+        'item' => 
+        array (
+          'HomeImg/index' => '首页图片管理',
+        ),
+      ),
+      1 => 
+      array (
+        'item' => 
+        array (
+          'HomeImg/add' => '添加首页图片',
+        ),
+      ),
+      2 => 
+      array (
+        'item' => 
+        array (
+          'HomeImg/edit' => '编辑首页图片',
+        ),
+        'hidden' => true,
+      ),
+    ),
+  ),
+  'ProduceImg' => 
+  array (
+    'name' => '产品图片管理',
+    'target' => 'ProduceImg/index',
+    'sub_menu' => 
+    array (
+      0 => 
+      array (
+        'item' => 
+        array (
+          'ProduceImg/index' => '产品图片管理',
+        ),
+      ),
+      1 => 
+      array (
+        'item' => 
+        array (
+          'ProduceImg/add' => '添加产品图片',
+        ),
+      ),
+      2 => 
+      array (
+        'item' => 
+        array (
+          'ProduceImg/edit' => '编辑产品图片',
+        ),
+        'hidden' => true,
+      ),
+    ),
+  ),
+  'HappySee' => 
+  array (
+    'name' => '喜闻乐见管理',
+    'target' => 'HappySee/index',
+    'sub_menu' => 
+    array (
+      0 => 
+      array (
+        'item' => 
+        array (
+          'HappySee/index' => '喜闻乐见管理',
+        ),
+      ),
+      1 => 
+      array (
+        'item' => 
+        array (
+          'HappySee/add' => '添加喜闻乐见',
+        ),
+      ),
+      2 => 
+      array (
+        'item' => 
+        array (
+          'HappySee/edit' => '编辑喜闻乐见',
+        ),
+        'hidden' => true,
+      ),
+    ),
+  ),
+  'HappyArticle' => 
+  array (
+    'name' => '乐见文章管理',
+    'target' => 'HappyArticle/index',
+    'sub_menu' => 
+    array (
+      0 => 
+      array (
+        'item' => 
+        array (
+          'HappyArticle/index' => '乐见文章管理',
+        ),
+      ),
+      1 => 
+      array (
+        'item' => 
+        array (
+          'HappyArticle/add' => '添加乐见文章',
+        ),
+      ),
+      2 => 
+      array (
+        'item' => 
+        array (
+          'HappyArticle/edit' => '编辑乐见文章',
+        ),
+        'hidden' => true,
+      ),
+    ),
+  ),
+);
