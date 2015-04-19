77c5850167f0a67f97a8e5b2e4ab1ee5fd79f723
simon <mahuan1768@126.com>
2015-04-18 23:52:44 -0700

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   Common/Conf/system_config.php

--- Common/Conf/system_config.php
+++ Common/Conf/system_config.php
@@ -1,9 +1,9 @@
 <?php return array (
   'DB_TYPE' => 'mysql',
-  'DB_HOST' => '182.92.212.222',
-  'DB_NAME' => 'yuetangdata',
-  'DB_USER' => 'root',
-  'DB_PWD' => 'mahuan6',
+  'DB_HOST' => 'mysql.sql89.cdncenter.net',
+  'DB_NAME' => 'sq_yuetang',
+  'DB_USER' => 'sq_yuetang',
+  'DB_PWD' => 'yuetangad123',
   'DB_PORT' => '3306',
   'DB_PREFIX' => 'ea_',
   'SITE_TITLE' => '下午好，奶茶！',
